21f311a3f6f00c06ec14730aea3562a214962dbf
Alexander Bazo <alexanderbazo@googlemail.com>
2019-10-10 23:32:02 +0200

    Add test project for new Bouncer library

--- src/BouncerDemo/DieStrasse.java
+++ src/BouncerDemo/DieStrasse.java
@@ -1,6 +1,8 @@
 package BouncerDemo;
 
 import de.ur.mi.bouncer.apps.BouncerApp;
+import de.ur.mi.bouncer.world.fields.FieldColor;
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
 public class DieStrasse extends BouncerApp {
 
@@ -8,6 +10,21 @@ public class DieStrasse extends BouncerApp {
 	@Override
 	public void bounce() {
 		loadMap("Strasse");
+		repairField();
+		while (bouncer.canMoveForward()) {
+				repairField();
+				bouncer.move();
+		}
+	}
+
+	private void repairField() {
+		if(bouncer.isOnFieldWithColor(FieldColor.RED)) {
+			bouncer.paintField(FieldColor.GREEN);
+		}
+	}
+
+	public static void main(String[] args) {
+		GraphicsAppLauncher.launch("DieStrasse");
 	}
 
 }
