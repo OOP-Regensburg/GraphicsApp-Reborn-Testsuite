a6ce3c93cec4e7ed8dc0aee2332155ad5f3ca98c
FS <florin.schwappach@ur.de>
2019-10-10 10:30:31 +0200

    Added basic File IO Demo.

--- src/FileDemo/FileDemoLauncher.java
+++ src/FileDemo/FileDemoLauncher.java
@@ -1,9 +1,37 @@
 package FileDemo;
 
-import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileDemoLauncher {
+    private static void saveAFile() {
+        String myPath = "data/test.txt";
+        try(FileWriter fileWriter = new FileWriter(myPath)) {
+            String fileContent = "OOP ist ein wichtiger und wertvoller Bestandteil einer Informatik-Diät.";
+            fileWriter.write(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void loadAFile() {
+        String myPath = "data/test.txt";
+        try(FileReader fileReader = new FileReader(myPath)) {
+            int ch = fileReader.read();
+            while(ch != -1) {
+                System.out.print((char)ch);
+                ch = fileReader.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
-        GraphicsAppLauncher.launch("FileDemo");
+        loadAFile();
+        saveAFile();
     }
 }
